40cb87aec80faf05379583951c53f18a68478dd6
nagatani-ay <nagatani@ay-sys.com>
2022-05-25 13:20:47 +0900

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,64 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState(['test1']);
+  const [todos, setTodos] = useState([]);
   const [isOpenMenu, toggleMenu] = useState(false);
+  const todolist = todos.map((todo) => <li key={todo}> {todo}</li>);
+
+  function test(b) {
+    if (b) {
+      return <p>text</p>;
+    } else {
+      return <span>text</span>;
+    }
+  }
+  // HTMLの中に書く場合{isOpenMenu && <p>test</p>}
+  // 三項演算子{isOpenMenu ? <p>true</p> : <p>false</p>}
+  function addTodo() {
+    setTodos((todos) => [...todos, { code:"",text: 'test' + todos.length }]);
+  }
+
+  function removeTodo() {
+    setTodos(todos.filter((todo, index) => todo == 'test0'));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        {/* v-ifの代わり*/}
-        {isOpenMenu && <p>test</p>}
+      <p>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
+          count is: {count}
+        </button>
+      </p>
 
-        {/* v-ifの代わり 三項演算子 */}
-        {isOpenMenu ? <p>true</p> : <p>false</p>}
-        <p>
-          <button
-            type="button"
-            onClick={() => toggleMenu((isOpenMenu) => !isOpenMenu)}
-          >
-            isOpenMenu is: {isOpenMenu + ''}
-          </button>
-        </p>
+      <p>
+        <button
+          type="button"
+          onClick={() => toggleMenu((isOpenMenu) => !isOpenMenu)}
+        >
+          isOpenMenu is: {isOpenMenu + ''}
+        </button>
+      </p>
 
-        <p>
-          <div>{todos}</div>
-          <button
-            type="button"
-            onClick={() => {
-              // pushはなどは再描画されないのでスプレット構文を使用して新たに配列を作成して保存
-              setTodos([...todos, 'test2']);
-            }}
-          >
-            add
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              // 削除はfillter
-              setTodos(todos.filter((todo, index) => todo !== 'test2'));
-            }}
-          >
-            remove
-          </button>
-        </p>
-      </header>
+      <p>
+        <button
+          type="button"
+          onClick={() => {
+            // pushはなどは再描画されないのでスプレット構文を使用して新たに配列を作成して保存
+            addTodo();
+          }}
+        >
+          add
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            // 削除はfillter
+            removeTodo();
+          }}
+        >
+          remove
+        </button>
+      </p>
+      <ul>{todolist}</ul>
     </div>
   );
 }
